06609c22c2b314953e0363f4105eb3bb80a14845
NK <guskova.ns@yandex.ru>
2023-03-30 15:05:34 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://1b8c8108-2023-4d05-b9b4-646c037a71a6.serverhub.praktikum-services.ru'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
